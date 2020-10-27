5ac18057955bd6ab1f30e3f8e1b9fa0d8c91a3e6
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2020-10-27 17:37:24 +0100

    reorg libs

--- libs/logs.py
+++
@@ -1,9 +0,0 @@
-import os, logging
-from libs import config 
-
-LOG_LEVEL = config.LOG_LEVEL
-
-LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
-              '-35s %(lineno) -5d: %(message)s')
-LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
